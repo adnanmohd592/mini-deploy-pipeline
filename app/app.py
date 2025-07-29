db7972ee6a529d40b48d34748243a573d024b985
adnanmohd592 <adnanmohd8341@gmail.com>
2025-07-29 15:04:10 +0530

    update app

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
      return "Flask App Deployed with CI/CD pipeline!"
-     
+     return "New version deployed!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
