253539969a1bb61d6d523a669aa164945d875b93
adnanmohd592 <adnanmohd8341@gmail.com>
2025-07-29 11:28:05 +0530

    initial commit

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = flask(__name__)
 @app.route('/')
 def home():
      return "Hello from the automated deployment pipeline!"
-     return "massage"
+     
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
